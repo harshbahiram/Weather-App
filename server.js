7b6138d87a0c7c6c59ed633b17ac1ca288a62733
harshbahiram <harshbahiram975@gmail.com>
2026-03-24 00:43:06 +0530

    Add button effects

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 
 const apiKey = process.env.WEATHER_API_KEY;
 
-
-
 app.use(express.static(__dirname));
 
 // very simple route
@@ -16,22 +14,12 @@ app.get('/weather', async (req, res) => {
             return res.status(400).json({ message: "City is required" });
         }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data.cod !== 200) {
-            return res.status(404).json({ message: data.message });
-        }
-
-    res.json({
-        name: data.name,
-        temp: data.main.temp,
-        humidity: data.main.humidity,
-        wind: data.wind.speed,
-        weather: data.weather[0].main
-    }); 
+    res.send(data);
 });
 
 app.listen(3000, () => {
